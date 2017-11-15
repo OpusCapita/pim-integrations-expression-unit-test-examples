17bb989563f921b28f2483ecda6b4edb9656c7d5
Alexej Fröbel <Alexej.Froebel@opuscapita.com>
2017-11-15 11:17:59 +0100

    doc: deleated useless comment

--- example/example.js
+++ example/example.js
@@ -10,20 +10,6 @@ let contextLanguage = util.contextLanguage
 let contextTag = util.contextTag
 let expect = require("chai").expect
 
-
-/*
-* At this point, your expressions are available. You can call them either
-* directly, or from within your test cases. contextLanguage is a simple
-* variable, which you can change. In expressions it is used for example with:
-* "en_EN","de_DE" or "es_ES".
-* term, boilerplate and product.getAttributeValue are sinon-stubs, and can be
-* configured like any other sinon stub. You can read the sinon documentation
-* here: http://sinonjs.org/
-* You can look up many examples at our example repo:
-* https://github.com/OpusCapita/stprj-expression-unit-test-examples
-* Happy testing!
-*/
-
 describe("It will not throw errors if you are using the expression functions", function(){
   it("calling term()",function(){
     getTerm(3)
